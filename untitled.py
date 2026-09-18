6f7aa3fd13ceff5cb07e57f31129fc55cb12c809
tiraelmefisto12 <tirael.mefisto.12@gmail.com>
2026-09-18 15:00:29 -0500

    Agrega grafo del mapa (Almacen, Centro, Terminal, Mercado, ZonaNorte)

---
+++ untitled.py
@@ -0,0 +1,11 @@
+import numpy as np
+import pandas as pd
+import matplotlib
+import sklearn
+import scipy
+
+print("NumPy:", np.__version__)
+print("Pandas:", pd.__version__)
+print("Matplotlib:", matplotlib.__version__)
+print("Scikit-learn:", sklearn.__version__)
+print("SciPy:", scipy.__version__)
